626864dd66e332aa7b5512de2ab231c562fb6588
Owen Versteeg <owen.versteeg@live.com>
2013-12-01 23:23:56 -0500

    add energy conversions

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function enlargedHex(hex) {
 
 function getColorOrWavelength(doNotTrack) {
   var value = document.getElementById('inputBox').value;
-  var color, direction, error;
+  var color, direction, error, frequency, energy;
   var nanometers = 0;
   
   if (parseFloat(value) && parseFloat(value).toString().length == value.toString().length) {
@@ -31,17 +31,26 @@ function getColorOrWavelength(doNotTrack) {
     nanometers = value;
     color = getColorFromWaveLength(nanometers);
     frequency = (299792458*1000000000)/nanometers;
+    energy = 6.62606957e-34 * frequency;
     direction = "fromNanometers";
   }
-  else if ((value.indexOf('e') != -1 || value.indexOf('^') != -1) && value.indexOf('14') != -1 && eval(value)>1e14 && eval(value)<1e15) {
+  else if (value.indexOf('e') != -1 && (value.indexOf('14') != -1 || value.indexOf('-19') != -1 && )) {
     //I just evalled unsanitized user input. twice. dealwithit.gif
-    //we have a frequency
-    //frequency = speed of light/wavelength
-    //wavelength = speed of light/frequency
-    var frequency = eval(value);
-    var nanometers = (299792458*1000000000)/frequency; //speed of light times a big number (to convert from m to nm)
+    //we have a number in sci-notation
+    if (eval(value)>1e14 && eval(value)<1e15) {
+      //frequency = speed of light/wavelength
+      //wavelength = speed of light/frequency
+      frequency = eval(value);
+      energy = 6.62606957e-34 * frequency;
+      direction = "fromFrequency";
+    }
+    else if (eval(value)>1e-20 && eval(value)<1e-18)  {
+      energy = eval(value);
+      frequency = energy/6.62606957e-34;
+      direction = "fromEnergy"
+    }
+    nanometers = (299792458*1000000000)/frequency; //speed of light times a big number (to convert from m to nm)
     color = getColorFromWaveLength(nanometers);
-    direction = "fromFrequency";
   }
   else {
     direction = "fromColor"
@@ -63,12 +72,13 @@ function getColorOrWavelength(doNotTrack) {
     
     if (nanometers != 0) {
     	frequency = (299792458*1000000000)/nanometers;
+    	energy = 6.62606957e-34 * frequency;
     }
   }
   document.getElementById('outputColor').style.backgroundColor = color;
    
   if (nanometers != 0) { 
-    document.getElementById('outputText').innerHTML = nanometers + " nanometers = "+color+" = "+Math.round(1000*frequency/1e14)/1000 + "e14 inverse seconds";
+    document.getElementById('outputText').innerHTML = nanometers + " nanometers = "+color+" = "+Math.round(1000*frequency/1e14)/1000 + "e14 inverse seconds = " + Math.round(1000*energy/1e-19)/1000 +"e-19 J";
     error = "none";
   } else {
     document.getElementById('outputText').innerHTML = "Sorry! Your chosen color does not exist as one wavelength of light! Color:"+color; 
